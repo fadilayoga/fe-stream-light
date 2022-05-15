4ad2655e77c97920bb225fcef899d2efc06cd6ef
fadilayoga <fadilayoga@users.noreply.github.com>
2022-05-15 12:32:13 +0700

    socket

--- src/globals/config.js
+++ src/globals/config.js
@@ -7,7 +7,7 @@ const CONFIG = {
     APP_ID: "1:234164185380:web:9b2d2d59e50c4eec096bb1",
     VAPID_KEY: "BBmItBj7schdRO2f1GzT1gX9Hion4WRo0Wg3ferJTSO45zOY6wAWaE4B358HWtL1DwCwsf_IIHSJ-1UnWMmtgfQ",
     BASE_URL: "https://lightstream.site/",
-    WEB_SOCKET: "wss://lightstream.site:3000/"
+    WEB_SOCKET: "wss://lightstream.site/"
   };
 
 export default CONFIG
