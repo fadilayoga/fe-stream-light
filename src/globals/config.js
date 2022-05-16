f54e07e29eb9b3fb766b743ca8cdae962ad7242d
fadilayoga <fadilayoga@users.noreply.github.com>
2022-05-16 09:53:45 +0700

    base url

--- src/globals/config.js
+++ src/globals/config.js
@@ -7,7 +7,7 @@ const CONFIG = {
     APP_ID: "1:234164185380:web:9b2d2d59e50c4eec096bb1",
     VAPID_KEY: "BBmItBj7schdRO2f1GzT1gX9Hion4WRo0Wg3ferJTSO45zOY6wAWaE4B358HWtL1DwCwsf_IIHSJ-1UnWMmtgfQ",
     BASE_URL: "http://135.148.157.108/",
-    WEB_SOCKET: "ws://135.148.157.108:3000/"
+    WEB_SOCKET: "wss://135.148.157.108:3000/"
   };
 
 export default CONFIG
